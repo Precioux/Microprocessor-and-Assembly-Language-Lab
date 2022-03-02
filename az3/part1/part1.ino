64e984bee57e129078ea2e430c158e49e0468329
Precioux <uni.mahdipour@gmail.com>
2022-03-03 00:55:00 +0330

    Project 3 part 1,3

---
+++ az3/part1/part1.ino
@@ -0,0 +1,43 @@
+#include <Time.h>
+#include <TimeLib.h>
+#include <LiquidCrystal.h>
+
+const int RS = 12, EN = 11, D4 = 5, D5 = 4, D6 =3, D7 = 2;
+LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
+
+void setup() {
+lcd.begin(16, 2);
+setTime(12,0,0,2,3,22);  
+lcd.setCursor(0, 0);     
+lcd.print(year());
+lcd.print("-");
+lcd.print(month());
+lcd.print("-");
+lcd.print(day());
+
+int i=9;
+int j=0;
+while(true){
+lcd.setCursor(i,j);
+lcd.print(".");
+delay(1000);
+i++;
+if(i==15){
+  if(j==1)
+  {
+    j=0;
+    i=0;
+  }
+  else{ j++;
+i=0;
+  }
+}
+
+}
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- az3/part3/part3.ino
+++ az3/part3/part3.ino
@@ -1,36 +1,67 @@
-#include <Time.h>
-#include <TimeLib.h>
+#include <StringSplitter.h>
 #include <LiquidCrystal.h>
-
+#include <string.h>
 const int RS = 12, EN = 11, D4 = 5, D5 = 4, D6 =3, D7 = 2;
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
 void setup() {
 lcd.begin(16, 2);
-setTime(12,0,0,2,3,22);  
-lcd.setCursor(0, 0);     
-lcd.print(year());
-lcd.print("-");
-lcd.print(month());
-lcd.print("-");
-lcd.print(day());
-
-int i=9;
-int j=0;
-while(true){
-lcd.setCursor(i,j);
-lcd.print(".");
-delay(1000);
-if(i==15){
-j++;
-i=0;
-}
-i++;
+Serial.begin(9600);  
 }
 
+void loop(){
+  while(Serial.available() > 0 ){
+    String str = Serial.readString();
+    printer(str,0);
+    for(int i=0;i<str.length();i++)
+    {
+      int n=str[i];
+      if(n<48 && n>41)
+      {
+       splitAndSolve(str,str[i]);
+      }
+    }
+  }
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void splitAndSolve(String str,char t){
+    int a[2];
+        int ans=0;
+        StringSplitter *splitter = new StringSplitter(str, t, 2);
+        int itemCount = splitter->getItemCount();
+        for(int j = 0; j < itemCount; j++){
+         String item = splitter->getItemAtIndex(j);
+         a[j]=item.toInt();
+        }
+        int n=t;
+        
+        switch(n){
+          case 43:
+               ans=a[1]+a[0];
+               break;
+          case 45:
+               ans=a[0]-a[1];
+               break;
+          case 42:
+               ans=a[0]*a[1];
+               break;
+          case 47:
+               ans=a[0]/a[1];  
+               break;         
+        }
+        char answer[1];
+        itoa(ans,answer,10);
+        printer(answer,1);
+}
+void printer(String data,int mode)
+{
+  if(mode==0){
+  lcd.clear();
+  lcd.print(data);
+ // Serial.println(data);
+  }
+   if(mode==1){
+  lcd.setCursor(0,1);
+  lcd.print(data);
+  Serial.println(data);
+  }
 }
